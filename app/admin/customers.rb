f917c97e92266d4f5adc2cf1ae1af2ae515cbee5
DanDaMan23 <danlawrcons@gmail.com>
2020-11-05 17:28:17 -0600

    Added Images into the Active Storage

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,13 +1,16 @@
 ActiveAdmin.register Customer do
 
   permit_params :full_name, :phone_number, :email, :notes, :image
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+
+    f.inputs do
+      f.input :image, as: :file
+    end
+
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 
 end
