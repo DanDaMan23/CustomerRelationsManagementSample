ba1e536df9825197ace343e1d45b81aaafd879e7
DanDaMan23 <danlawrcons@gmail.com>
2020-11-05 17:01:04 -0600

    Added Active Storage

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,11 +1,6 @@
 ActiveAdmin.register Customer do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email, :notes
+  permit_params :full_name, :phone_number, :email, :notes, :image
   #
   # or
   #
@@ -14,5 +9,5 @@ ActiveAdmin.register Customer do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
