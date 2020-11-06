f7b712db16707bd12c129ed9276b178e3e303956
DanDaMan23 <danlawrcons@gmail.com>
2020-11-06 00:55:50 -0600

    Finished CRM Project

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,14 +1,17 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all 
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def alphabetized
+    @customers = Customer.all.order(full_name: :asc)
   end
 
   def missing_email
+    @customers = Customer.where(email: "")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'customers#index'
+
   get 'customers/index'
-  get 'customers/show'
+  # get 'customers/show'
   get 'customers/alphabetized'
   get 'customers/missing_email'
+
+
+  resources "customers", only: %i[show]
+
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
